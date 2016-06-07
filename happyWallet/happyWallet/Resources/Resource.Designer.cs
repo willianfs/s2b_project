d58aaf35aa45acc97882e23ee973a272c287653f
Xamarin <Xamarin@DESKTOP-KM07L7G>
2016-06-07 16:49:01 -0300

    Inicialização da criação do Layout.

--- happyWallet/happyWallet/Resources/Resource.Designer.cs
+++ happyWallet/happyWallet/Resources/Resource.Designer.cs
@@ -61,7 +61,19 @@ namespace happyWallet
 		{
 			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int lstSaldoGeral = 2131034112;
+			
+			// aapt resource value: 0x7f050001
+			public const int tableLayout1 = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int tvCredito = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int tvDebito = 2131034116;
+			
+			// aapt resource value: 0x7f050002
+			public const int tvSaldo = 2131034114;
 			
 			static Id()
 			{
@@ -77,7 +89,10 @@ namespace happyWallet
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int ActivityHappyWallet = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int layout_saldo_geral = 2130903041;
 			
 			static Layout()
 			{
