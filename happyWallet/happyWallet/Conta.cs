7f575efb322477a4f8d03a452ea19f3a69f0a115
Xamarin <Xamarin@DESKTOP-KM07L7G>
2016-06-08 13:53:28 -0300

    Implementando conexão com o banco de dados

--- happyWallet/happyWallet/Conta.cs
+++ happyWallet/happyWallet/Conta.cs
@@ -9,11 +9,13 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using SQLite;
 
 namespace happyWallet
 {
     class Conta
     {
+        [PrimaryKey, AutoIncrement]
         public int id_conta { get; set; }
         public String descricao { get; set; }
         public bool isValorNegativo { get; set; }
